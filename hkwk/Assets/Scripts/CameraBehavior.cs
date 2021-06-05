83c3eae13266fad021a2dd188c2c42c9234b62fb
tschuyebuhl <ksawerywr@gmail.com>
2021-06-05 23:05:28 +0200

    dodano fasolke ktora sie obraca, work in progress

---
+++ hkwk/Assets/Scripts/CameraBehavior.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehavior : MonoBehaviour
+{
+  //1
+  public Vector3 camOffset = new Vector3(0f, 1.2f, -2.6f);
+  //2
+  private Transform target;
+    void Start()
+    {
+    //3
+    target = GameObject.Find("Player").transform;
+    }
+  //4
+    void LateUpdate()
+    {
+    //5
+    this.transform.position = target.TransformPoint(camOffset);
+    //6
+    this.transform.LookAt(target);
+    }
+}
+/*
+ * 1. Deklaracja zmiennej Vector3 ktora przechowuje dystans pomiedzy Main Camera i *CAPSULE*, mozna recznie zmieniac x, y, i z. 
+ * 2.Zmienna ktora przechowuje informacje Transform gracza, access modifier na prywatny bo nie chce zeby bylo dostepne poza tą klasą
+ * 3. GameObject.Find znajduje GameObject o takiej nazwie i przechowuje informacje o pozycji x, y i z gracza w zmiennej target.
+ * 4. LateUpdate to jedna z metod klasy MonoBehaviour, wykonuje się PO update, a w update wykonuje się ruch gracza, wiec taka kolejnosc jest najlepsza
+ * 5. 
+ */
---
+++ hkwk/Assets/Scripts/PlayerBehavior.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehavior : MonoBehaviour
+{
+  //1
+  public float moveSpeed = 10f;
+  public float rotateSpeed = 75f;
+  //2
+  private float vInput;
+  private float hInput;
+
+    // Update is called once per frame
+    void Update()
+    {
+    //3
+    vInput = Input.GetAxis("Vertical") * moveSpeed;
+    //4
+    hInput = Input.GetAxis("Horizontal") * rotateSpeed;
+    //5
+    this.transform.Translate(Vector3.forward * vInput * Time.deltaTime);
+    //6
+    this.transform.Rotate(Vector3.up * hInput * Time.deltaTime);
+    }
+}
+/*1. dwie publiczne zmienne jako mnożniki: movespeed, czyli jak szybko gracz sie porusza przod/tyl
+ *2. rotatespeed czyli jak szybko gracz sie obraca lewo/prawo
+ *3. jest od wykrywania W/S 
+ *4. wykrywanie klawiszy A/D
+ *5. metoda Translate ktora przyjmuje jako parametr wektor3D zeby poruszyc komponent Transform gracza:
+ *  this użyto w celu specyfikacji GameObjectu do ktorego jest przyczepiony skrypt
+ *  Vector3 pomnozony przez Time.DeltaTime oraz vInput daje kierunek i predkosc w kierunku przod/tyl
+ *  Time.DeltaTime to czas od ostatniej klatki, uzywa sie go zeby uniezaleznic predkosc gracza od fps urzadzenia.
+ *6. Roatate w celu obrotu, refy do punktu 5. 
+ */
